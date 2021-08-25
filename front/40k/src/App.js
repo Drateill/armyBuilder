27adfb707e63527486c90f7ca54e0c4f34e00c36
Drateill <38911374+Drateill@users.noreply.github.com>
2021-08-26 00:42:23 +0200

    Commentaire + Changement d'ui

--- front/40k/src/App.js
+++ front/40k/src/App.js
@@ -7,11 +7,14 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Sticky from 'react-stickynode';
 
+const _ = require('lodash');
+
 
 function App() {
 
   const [weaponsList, setweaponsList] = useState([])
   const [modelsList, setmodelsList] = useState([])
+  const [typeSelected, settypeSelected] = useState([])
   const [list, setlist] = useState([])
   const [Cost, setCost]= useState(0)
   const [Power, setPower]=useState(0)
@@ -48,6 +51,14 @@ function App() {
     setPower(Power - modelsList.filter(item => item.Model ===model )[0].Power)
 
   }
+
+  const handletypeSelected= (e)=>{
+    settypeSelected(e.target.value)
+  }
+
+  var grouped = _(modelsList).groupBy('Type').map((model, type)=>({type: type, model:_.map(model, 'Model')})).value();
+
+
   var n = 0;
 
   if(weaponsList.length===0){
@@ -58,9 +69,36 @@ function App() {
   return (
     <div className="App">
       <HEADER></HEADER>
+{/* Selecteur de type  */}
+<select onChange={handletypeSelected}>
+<option value="">---Select a type---</option>
 
-        
+  {
+    grouped.map((item)=>{
+      return(
+        <option value={item.type}>{item.type}</option>
+      )
+    })
+  }
+</select>
+{/* ================== */}
+
+{/* Affichage des figurines par rapport au type selectionné */}
+{
+  modelsList.filter(obj=> obj.Type===typeSelected).map((model)=>{
+    return(
+      <MODEL
+          model={model}
+          key={model.Model}
+          addToList={addToList}
+          />
+    )
+  })
+}     
+{/*==========================================================*/}
       <div className="Container">
+
+        {/* Affichage temporaire de la composition de l'armée */}
         <Sticky enabled={true} top="#header" bottomBoundary="#content" className="Sticky">
         Total Cost : {Cost} Point - Power level : {Power}
           {
@@ -79,22 +117,7 @@ function App() {
             ""
           }
             </Sticky>
-
-        
-        <div className="modelList">
-          {
-            modelsList.map((model)=>{
-              return(
-          <MODEL
-          model={model}
-          key={model.Model}
-          addToList={addToList}
-          />
-          
-              )
-            })
-          }
-        </div>
+            {/* ============================================= */}
         </div>
       </div>
   );
